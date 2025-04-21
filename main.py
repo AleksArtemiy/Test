ae3191a9c85355535ad1943955b3dd9bd0602f32
aleks <alekseev3artem@gmail.com>
2025-04-21 17:46:15 +0300

    Test2

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
 import torch
 import numpy as np
-print("CUDA доступна: ", torch.cuda.is_available())
-print("Девайс: ", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "CPU")
+print("Hello world!")
